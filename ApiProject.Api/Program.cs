35146682c959aa38237feede2e6fbfeacb01bf97
Fatmaaktar <fatmaaktar214@gmail.com>
2024-10-03 21:55:05 +0300

    🛠️ Configuration completed and DbContext created

--- ApiProject.Api/Program.cs
+++ ApiProject.Api/Program.cs
@@ -1,3 +1,5 @@
+using Api.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +13,8 @@ var emv = builder.Environment;
 builder.Configuration.SetBasePath(emv.ContentRootPath)
     .AddJsonFile("appsettings.json", optional: false)
     .AddJsonFile($"appsettings.{emv.EnvironmentName}.json", optional:false);
+
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 

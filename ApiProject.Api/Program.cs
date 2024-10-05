2cb5399acc4dce6503cdf4e8c69fd075b5d0408b
Fatmaaktar <fatmaaktar214@gmail.com>
2024-10-05 22:43:36 +0300

    ✨ Created GetAllProductsQuery and related handler

--- ApiProject.Api/Program.cs
+++ ApiProject.Api/Program.cs
@@ -1,4 +1,5 @@
 using Api.Persistence;
+using Api.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,7 @@ builder.Configuration.SetBasePath(emv.ContentRootPath)
     .AddJsonFile($"appsettings.{emv.EnvironmentName}.json", optional:false);
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 

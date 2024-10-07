47040f508923aeb6f128c22c00e26d7ef040976f
Fatmaaktar <fatmaaktar214@gmail.com>
2024-10-07 22:54:14 +0300

    🚀 The AutoMapper development is completed!

--- ApiProject.Api/Program.cs
+++ ApiProject.Api/Program.cs
@@ -1,5 +1,7 @@
 using Api.Persistence;
 using Api.Application;
+using ApiProject.Mapper;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,7 @@ builder.Configuration.SetBasePath(emv.ContentRootPath)
 
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddApplication();
+builder.Services.AddCustomMapper();
 
 var app = builder.Build();
 
